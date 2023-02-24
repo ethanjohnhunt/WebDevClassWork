2a8564ba2286de54c03521eb9f6f8a2fb07ecef9
ethanjohnhunt <115984468+ethanjohnhunt@users.noreply.github.com>
2023-02-24 10:14:49 +0000

    Pushed
    Made some changes trying to make an onclick change background color

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function printText() {
     const x = document.getElementById("name");
   document.querySelector(".inputValue").innerHTML = x.value
 }
+
+function changeBackground(){
+    alert("somethingClicked")
+}
